cec85ddf94de419c8bcdbbcf487f60bad5aaca68
SrikrishnaKidambi <kidambivenkatasrikrishna@gmail.com>
2024-10-27 17:55:36 +0530

    contribute page added

--- homepage/script.js
+++ homepage/script.js
@@ -1,5 +1,6 @@
 const loginbtn=document.getElementById('loginbtn');
 const requestFeature=document.getElementById('requestFeature');
+const contributeQuestions=document.getElementById('contribute');
 
 loginbtn.addEventListener('click',()=>{
     window.location.href="../loginPage/index.html";
@@ -7,4 +8,8 @@ loginbtn.addEventListener('click',()=>{
 
 requestFeature.addEventListener('click',()=>{
     window.location.href="./RequestFeature/rf.html";
+})
+
+contributeQuestions.addEventListener('click',()=>{
+    window.location.href="./ContributeToQuiz/index.html";
 })
