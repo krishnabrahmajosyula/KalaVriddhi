9fa392713f76708a980c00af518f1eccdf437c28
SrikrishnaKidambi <kidambivenkatasrikrishna@gmail.com>
2024-11-03 19:54:22 +0530

    basic setup for admin page

--- loginPage/script.js
+++ loginPage/script.js
@@ -1,6 +1,7 @@
 const passwordInput=document.getElementById('password');
 const togglePassword=document.getElementById('togglePassword');
 const loginForm=document.getElementById('loginForm');
+const adminCheck=document.getElementById('adminCheck');
 togglePassword.addEventListener('click',()=>{
     const type=passwordInput.getAttribute('type')==='password'?'text':'password';
     passwordInput.setAttribute('type',type);
@@ -11,12 +12,26 @@ togglePassword.addEventListener('click',()=>{
 
 loginForm.addEventListener('submit',(event)=>{
     event.preventDefault();
-    if(Validate()){
-        window.location.href="../homepage/index.html"
+    if(adminCheck.checked){
+        console.log("Admin checkbox is checked. Redirecting to admin page...");
+        if(Validate1()){
+            window.location.href="../adminPage/index.html"
+        }
+    }
+    else{
+        console.log("User checkbox is checked. Redirecting to home page...");
+        if(Validate()){
+            window.location.href="../homepage/index.html"
+        }
     }
 })
 
 function Validate(){
+    //backend logic comes here
+    return true;
+}
+
+function Validate1(){
     //backend logic comes here
     return true;
 }
